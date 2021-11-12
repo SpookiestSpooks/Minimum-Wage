4620d7466ae0155b98d3471692208a801a29a2a0
FG-Robert <robert.aselid@gmail.com>
2021-11-12 18:07:09 +0100

    Bounce Sound Added

--- Assets/Scenes/Robert/Scripts/TrampolineBounch.cs
+++ Assets/Scenes/Robert/Scripts/TrampolineBounch.cs
@@ -10,16 +10,24 @@ public class TrampolineBounch : MonoBehaviour
     [SerializeField] float force = 10f;
     [SerializeField] float radius = 10f;
 
+    // Audio Related
+    AudioSource audioSource;
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
-       
-
         if (other.gameObject.CompareTag("Player"))
         {
+            audioSource.Play();
+
             Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
 
              rb.AddExplosionForce(force, transform.position, radius); 
         }
-
     }
 }
