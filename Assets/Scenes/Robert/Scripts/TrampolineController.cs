c438fe749d3349b5d6a7bb028ae1bccd96f2520e
FG-Robert <robert.aselid@gmail.com>
2021-11-12 10:53:03 +0100

    Whatever

---
+++ Assets/Scenes/Robert/Scripts/TrampolineController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolineController : MonoBehaviour
+{
+    CharacterController pController;
+
+    float xMovement;
+    [SerializeField] float speed = 5f;
+
+
+
+
+    void Start()
+    {
+        pController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        xMovement = Input.GetAxis("Horizontal");
+
+
+        pController.Move(new Vector3(xMovement, 0f, 0f) * speed * Time.deltaTime);
+
+    }
+}
