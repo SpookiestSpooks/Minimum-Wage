5e37e67f2a22d60308b7ded15b6fb6d3e7604103
FG-Robert <robert.aselid@gmail.com>
2021-11-12 11:02:44 +0100

    32

--- Assets/Scenes/Robert/Scripts/TrampolineController.cs
+++ Assets/Scenes/Robert/Scripts/TrampolineController.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class TrampolineController : MonoBehaviour
 {
+    // Trampoline Control
     CharacterController pController;
 
     float xMovement;
     [SerializeField] float speed = 5f;
 
+
+    // Explosion/Bounch Related
+    float force = 10f;
+    float radius = 10f;
 
 
 
@@ -25,5 +30,17 @@ public class TrampolineController : MonoBehaviour
 
         pController.Move(new Vector3(xMovement, 0f, 0f) * speed * Time.deltaTime);
 
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Hello");
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+
+            rb.AddExplosionForce(force, transform.position, radius);
+        }
     }
 }
